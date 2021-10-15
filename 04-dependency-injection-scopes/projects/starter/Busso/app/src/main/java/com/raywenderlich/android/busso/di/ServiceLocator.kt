e0d163a252a3fa9e9f5388134f570a1612f1ce34
Alex Alexeyev <aasmc@yandex.ru>
2021-10-15 22:10:28 +0300

    Add ActivityServiceLocator

--- 04-dependency-injection-scopes/projects/starter/Busso/app/src/main/java/com/raywenderlich/android/busso/di/ServiceLocator.kt
+++ 04-dependency-injection-scopes/projects/starter/Busso/app/src/main/java/com/raywenderlich/android/busso/di/ServiceLocator.kt
@@ -38,8 +38,10 @@ package com.raywenderlich.android.busso.di
  * This is the abstraction for the ServiceLocator design pattern implementation
  */
 interface ServiceLocator {
-  /**
-   * Returns the object of type A bound to a specific name
-   */
-  fun <A : Any> lookUp(name: String): A
+    /**
+     * Returns the object of type A bound to a specific name
+     */
+    fun <A : Any> lookUp(name: String): A
 }
+
+typealias ServiceLocatorFactory<A> = (A) -> ServiceLocator
--- 04-dependency-injection-scopes/projects/starter/Busso/app/src/main/java/com/raywenderlich/android/busso/di/ServiceLocatorImpl.kt
+++ 04-dependency-injection-scopes/projects/starter/Busso/app/src/main/java/com/raywenderlich/android/busso/di/ServiceLocatorImpl.kt
@@ -41,6 +41,7 @@ import com.raywenderlich.android.busso.permission.GeoLocationPermissionCheckerIm
 import com.raywenderlich.android.location.rx.provideRxLocationObservable
 
 const val LOCATION_OBSERVABLE = "LocationObservable"
+const val ACTIVITY_LOCATOR_FACTORY = "ActivityLocatorFactory"
 
 /**
  * Implementation for the ServiceLocator interface
@@ -49,15 +50,32 @@ class ServiceLocatorImpl(
     val context: Context
 ) : ServiceLocator {
 
-  private val locationManager =
-      context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-  private val geoLocationPermissionChecker = GeoLocationPermissionCheckerImpl(context)
-  private val locationObservable =
-      provideRxLocationObservable(locationManager, geoLocationPermissionChecker)
+    private val locationManager =
+        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    private val geoLocationPermissionChecker = GeoLocationPermissionCheckerImpl(context)
+    private val locationObservable =
+        provideRxLocationObservable(locationManager, geoLocationPermissionChecker)
 
-  @SuppressLint("ServiceCast")
-  override fun <A : Any> lookUp(name: String): A = when (name) {
-    LOCATION_OBSERVABLE -> locationObservable
-    else -> throw IllegalArgumentException("No component lookup for the key: $name")
-  } as A
+    @Suppress("IMPLICIT_CAST_TO_ANY", "UNCHECKED_CAST")
+    @SuppressLint("ServiceCast")
+    override fun <A : Any> lookUp(name: String): A = when (name) {
+        LOCATION_OBSERVABLE -> locationObservable
+        ACTIVITY_LOCATOR_FACTORY -> activityServiceLocatorFactory
+        else -> throw IllegalArgumentException("No component lookup for the key: $name")
+    } as A
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
